b8b85142d72c25d80bbcbff35e1ba74dc07a7abf
Thiago Leite <thiagofmleite@gmail.com>
2024-04-29 18:56:12 -0300

    List all meals route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 from database import db
 from models.meal import Meal
 
@@ -7,6 +7,12 @@ app.config['SECRET_KEY'] = "your_secret_key"
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:admin123@127.0.0.1:3306/daily-diet'
 
 db.init_app(app=app)
+
+
+@app.route('/meals', methods=['GET'])
+def get_meals():
+    meals = Meal.query.all()
+    return jsonify({'meals': meals, 'total': len(meals)})
 
 
 if __name__ == '__main__':
